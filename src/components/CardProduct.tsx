c6c968d557711298edaa426078043bb18977cbfb
fAEDKAN <sfdk.94@hotmail.com>
2023-02-05 05:41:41 -0300

    files deleted

--- src/components/CardProduct.tsx
+++
@@ -1,9 +0,0 @@
-const CardProduct = (props : any) => {
-    return (
-        <div>
-            <h2>{props.product.name}</h2>
-            <b>{props.product.price}</b>
-        </div>
-    );
-};
-export default CardProduct;
